94410af454329a2750f06666953be4ddbc2c0d9e
Balikis Moromoke Oyeleye <96819668+Qreamville@users.noreply.github.com>
2022-08-07 20:21:14 -1200

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
+contract TodoList {
+
+    struct Todo{
+        string text;
+        bool isComplete;
+    }
+
+    Todo[] public todos;
+
+    function creatTodo(string calldata _text) external{
+        todos.push(Todo(_text, false));
+    }
+
+    function editTodo(uint _index, string calldata _newText) external{
+        todos[_index].text = _newText;
+    }
+
+    function completed(uint _index) external{
+        todos[_index].isComplete = !todos[_index].isComplete;
+    }
+}
