49487fb1e3c99fbeb742bfe26f82f7435057159d
Balikis Moromoke Oyeleye <96819668+Qreamville@users.noreply.github.com>
2022-08-18 22:14:15 -1200

    Update MultiSignature.sol

--- MultiSignature.sol
+++ MultiSignature.sol
@@ -28,6 +28,20 @@ contract MultiSignatureWallet {
         _;
     }
 
+    modifier txExists(uint _txId){
+        require(_txId < transactions.length, "Transaction does not exist");
+        _;
+    }
+
+    modifier notApproved(uint _txId){
+        require(!approvedByOwner[_txId][msg.sender], "Transaction already approved");
+        _;
+    }
+    modifier notExecuted(uint _txId){
+        require(!transactions[_txId].executed, "Transaction already executed");
+        _;
+    }
+
     constructor(address[] memory _owners, uint _required) {
         require(_owners.length > 0, "Owners required");
         require(_required > 0 && _required <= _owners.length, "Invalid required number of owners");
@@ -56,5 +70,37 @@ contract MultiSignatureWallet {
             data:_data,
             executed: false
         }));
+        emit Submit(transactions.length - 1);
+    }
+
+    function approve(uint _txId) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId) {
+        approvedByOwner[_txId][msg.sender] = true;
+        emit Approve(msg.sender, _txId);
+    }
+
+    function getApprovalCount(uint _txId) private view returns (uint count) {
+        for (uint i; i < owners.length; i++){
+            if (approvedByOwner[_txId][owners[i]]){
+                count += 1;
+            }
+        }
+    }
+
+    function executed(uint _txId) external txExists(_txId) notExecuted(_txId) {
+        require(getApprovalCount(_txId) >=  requiredSigners, "Approvals is less than required signers");
+        Transaction storage  transaction = transactions[_txId];
+        transaction.executed = true;
+
+        (bool sucess,) = transaction.to.call{value: transaction.value}(transaction.data);
+        require(sucess, "transaction failed");
+
+        emit Execute(_txId);
+    }
+
+    function revoke(uint _txId) external onlyOwner txExists(_txId) notExecuted(_txId){
+        require(approvedByOwner[_txId][msg.sender], "Transaction not approved");
+        approvedByOwner[_txId][msg.sender] = false;
+
+        emit Revoke(msg.sender, _txId);
     }
 }
